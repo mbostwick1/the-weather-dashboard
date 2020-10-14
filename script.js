3f05379524a6fe9a492845c8354e8c1ef885f6de
Melanie Bostwick <mbostwick1@gmail.com>
2020-10-14 14:58:06 -0400

    removed placeholder city name, added js, added ajax, accessing weather data upon search

--- script.js
+++ script.js
@@ -5,23 +5,42 @@
 var city = "";
 var searchInput = $("#search-input");
 var searchBtn = $("#search-btn");
+var userCity=[];
+var currentCity = $("#current-city");
+var currentTemp = $("#temperature");
+var currentHumidity= $("#humidity");
+var currentWindSpeed=$("#wind-speed");
+var currentUVIndex= $("#uv-index");
 
-
-function init() {}
+// CITY SEARCH //
+function searchCity(x){
+    for (var i = 0; i < userCity.length; i++){
+        if( x.toLowerCase() === userCity[i]){
+            return -1;
+        }
+    }
+    return 1;
+}
 
 // API KEY //
- var APIKey = "6c462ba73204a6931ad191705428066";
+ var APIKey = "a0aca8a89948154a4182dcecc780b513";
+
+// Show weather data //
+
+function showWeather(event){
+    event.preventDefault();
+    if( searchInput.val().trim() !== ""){
+        city = searchInput.val().trim();
+        getWeather(city);
+    }
+}
 
 // Get current city weather data: current date, temperature, humidity, wind speed, and UV Index //
-function getWeather() {
-
+function getWeather(city) {
 
  // Here we are building the URL we need to query the database
  var queryURL =
-   "https://api.openweathermap.org/data/2.5/weather?q=" +
-   city + "&appid="
-   APIKey +
-   "&units=imperial";
+   "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial";
 
 // We then created an AJAX call
 $.ajax({
@@ -40,4 +59,4 @@ $.ajax({
 // Save location searches local storage //
 
 // Search button/function //
-
+searchBtn.on("click", showWeather);
