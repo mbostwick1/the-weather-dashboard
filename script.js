332909a2367848067b271ce91b36074beff0bba0
Melanie Bostwick <mbostwick1@gmail.com>
2020-10-14 11:34:32 -0400

    updated html to include current and forecast, some css styling, and minor js

--- script.js
+++ script.js
@@ -1,25 +1,43 @@
 // console.log("oh hey!");
+ 
+// variables //
 
-var previousLocations = [];
+var city = "";
+var searchInput = $("#search-input");
+var searchBtn = $("#search-btn");
 
-function init() {
 
+function init() {}
 
-}
+// API KEY //
+ var APIKey = "6c462ba73204a6931ad191705428066";
 
 // Get current city weather data: current date, temperature, humidity, wind speed, and UV Index //
+function getWeather() {
 
+
+ // Here we are building the URL we need to query the database
+ var queryURL =
+   "https://api.openweathermap.org/data/2.5/weather?q=" +
+   city + "&appid="
+   APIKey +
+   "&units=imperial";
+
+// We then created an AJAX call
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+  });
+ 
+}
 
 //Get 5 day forecast for current city: date, weather icon, temperature, and humidity //
 
-
 // Clear weather data upon new search //
-
-
 
 // Save location searches local storage //
 
-
 // Search button/function //
-
 
