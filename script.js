f6be0d5bff7de4e75fd38218ffa3607f2f4df77d
Melanie Bostwick <mbostwick1@gmail.com>
2020-10-13 23:39:24 -0400

    pseudocode js

---
+++ script.js
@@ -0,0 +1,25 @@
+// console.log("oh hey!");
+
+var previousLocations = [];
+
+function init() {
+
+
+}
+
+// Get current city weather data: current date, temperature, humidity, wind speed, and UV Index //
+
+
+//Get 5 day forecast for current city: date, weather icon, temperature, and humidity //
+
+
+// Clear weather data upon new search //
+
+
+
+// Save location searches local storage //
+
+
+// Search button/function //
+
+
